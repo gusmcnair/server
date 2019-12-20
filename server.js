a04e5255727866b2b66e06efc1800f9c9d692c80
Angus McNair <amcnair234@gmail.com>
2019-12-19 22:35:45 -0600

    Commit with working server

---
+++ server.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const moviesdata = require('./movies-data.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({ error: 'Bad credentials'})
+    }
+
+    next();
+})
+
+app.get('/films', handleGetFilms)
+
+function handleGetFilms(req, res){
+
+    let convertedNumber = (Number(req.query.avgvote))
+
+    if(Number.isNaN(convertedNumber) || Number(req.query.avgvote) > 10 || Number(req.query.avgvote < 0)){
+        res.send(400, 'Invalid average vote input.')
+    }
+
+    let results = moviesdata;
+
+    if(req.query.genre){
+        results = results.filter(film => 
+            film.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
+    }
+
+    if(req.query.country){
+        results = results.filter(film => 
+            film.country.toLowerCase().includes(req.query.country.toLowerCase()))
+    }
+
+    if(req.query.avgvote){
+        results = results.filter(film => 
+            film.avg_vote >= Number(req.query.avgvote))
+    }
+
+    if (results.length === 0){results = (400, "No films to return.")}
+    res.send(results)
+
+}
+
+
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
+
